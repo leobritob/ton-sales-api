9a3dc24d21aa7c4776baac8bfec840f58308aaca
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 17:04:47 -0300

    feat: configured repository

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Ton Sales')
+    .setDescription('The Ton Sales API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.HTTP_PORT || 3333);
+}
+bootstrap();
