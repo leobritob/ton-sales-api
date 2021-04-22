a6431195ca034714b18dfab2d9230eb60707f846
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-22 11:21:39 -0300

    test(products): should be defined

--- src/app/products/products.service.spec.ts
+++ src/app/products/products.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { DynamoDBHelper } from '../../helpers/dynamodb.helper'
 import { ProductsService } from './products.service'
 
 describe('ProductsService', () => {
-  let service: ProductsService
+  let productsService: ProductsService
+  let dynamoDBHelper: DynamoDBHelper
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: DynamoDBHelper,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            updateItem: jest.fn(),
+            deleteItem: jest.fn(),
+          },
+        },
+      ],
     }).compile()
 
-    service = module.get<ProductsService>(ProductsService)
+    productsService = module.get<ProductsService>(ProductsService)
+    dynamoDBHelper = module.get<DynamoDBHelper>(DynamoDBHelper)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(productsService).toBeDefined()
+    expect(dynamoDBHelper).toBeDefined()
   })
 })
