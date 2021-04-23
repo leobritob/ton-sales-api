a011086e8babf614acc7a05c43533e6bb924ac6d
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-22 22:14:31 -0300

    feat: create health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ProductsModule } from './app/products/products.module'
 import { CountApiModule } from './app/count-api/count-api.module';
+import { HealthModule } from './app/health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule, CountApiModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule, CountApiModule, HealthModule],
   controllers: [],
   providers: [],
 })
